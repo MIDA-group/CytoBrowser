b600263161c93777f55b3e11b744e1a4488b3559
Christopher Rydell <stoffe@kiosan.com>
2020-06-30 19:32:29 +0200

    Server endpoint for getting all image names

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// Bind required modules
+// Declare required modules
+const fs = require("fs");
 const express = require("express");
 
 // Store the address to be used from arguments
@@ -17,6 +18,34 @@ server.use("/data", express.static("data"));
 // Serve the index page at the root
 server.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
+});
+
+// Get a list of available images
+server.get("/api/images", (req, res) => {
+    // Read the available files in the data directory
+    fs.readdir('./data', (err, dir) => {
+        if (err) {
+            console.log("Couldn't find the ./data diectory.");
+            res.status(500);
+            res.send("The server did not have a data directory.");
+        }
+        else {
+            // Find all unique image names in the directory
+            const nameEx = /[^(\.|^)]+_x40/g; // TODO: Haven't checked if this applies to all images
+            const names = dir.map((fileOrDirName) => {
+                const name = fileOrDirName.match(nameEx);
+                if (name === null) {
+                    return null;
+                }
+                else {
+                    return name[0];
+                }
+            });
+            const uniqueNames = [... new Set(names.filter((name) => name !== null)];
+            res.status(200);
+            res.json({imageNames: uniqueNames});
+        }
+    });
 });
 
 // Begin listening on the specified interface
