ee9e56cf75393559c43887e93c826225dfca0d34
Christopher Rydell <stoffe@kiosan.com>
2020-07-02 01:30:34 +0200

    Reduced calls to fs.readdir()

--- availableImages.js
+++ availableImages.js
@@ -12,81 +12,67 @@ let availableImages = null;
 const nameEx = /.+(?=_z[0-9]+\.dzi)/g;
 const zEx = /(?<=_z).*(?=\.dzi)/g;
 
-function getZLevels(image) {
-    fs.readdir("./data", (err, dir) => {
-        if (err) {
-            image.zLevels = [];
-            console.log(`Couldn't find z levels for ${image.name}`);
-            return;
-        }
-        // Only look at dzi files for the right name
-        const nameFilter = RegExp(`^${image.name}.*\.dzi$`);
-        const names = dir.filter((name) => nameFilter.test(name));
+function getZLevels(dir, image) {
+    // Only look at dzi files for the right name
+    const nameFilter = RegExp(`^${image.name}.*\.dzi$`);
+    const names = dir.filter((name) => nameFilter.test(name));
 
-        // Isolate the z levels in the dzi filenames
-        const zLevels = names.map((name) => name.match(zEx)).flat();
-        image.zLevels = zLevels.sort((a, b) => +a - +b);
-    });
+    // Isolate the z levels in the dzi filenames
+    const zLevels = names.map((name) => name.match(zEx)).flat();
+    image.zLevels = zLevels.sort((a, b) => +a - +b);
 }
 
-function getThumbnails(image) {
-    fs.readdir("./data", (err, dir) => {
-        if (err) {
-            image.thumbnails = {};
-            console.log(`Couldn't find thumbnails for ${image.name}`);
-            return;
-        }
-        // Find the file directories for the image name
-        const nameFilter = RegExp(`^${image.name}.*_files$`);
-        const names = dir.filter((name) => nameFilter.test(name));
+function getThumbnails(dir, image) {
+    // Find the file directories for the image name
+    const nameFilter = RegExp(`^${image.name}.*_files$`);
+    const names = dir.filter((name) => nameFilter.test(name));
 
-        // Look through the middle file directory
-        const fileDir = names[Math.floor(names.length / 2)];
-        fs.readdir(`./data/${fileDir}`, (err, dir) => {
-            // Sort the directories numerically
-            dir = dir.sort((a, b) => +a - +b);
+    // Look through the middle file directory
+    const fileDir = names[Math.floor(names.length / 2)];
+    fs.readdir(`./data/${fileDir}`, (err, dir) => {
+        // Sort the directories numerically
+        dir = dir.sort((a, b) => +a - +b);
 
-            // Look through each directory to find thumbnails
-            let idx = 0;
-            const thumbnails = {overview: null, detail: null};
-            image.thumbnails = thumbnails;
-            function findThumbnails(){
-                if (idx === dir.length) {
-                    return;
-                }
-                const path = `./data/${fileDir}/${dir[idx]}`;
-                fs.readdir(path, (err, dir) => {
-                    if (err) {
-                        // TODO: Handle errors
-                    }
-
-                    // Store suitable thumbnails
-                    if (dir.length === 1) {
-                        thumbnails.overview = `${path}/${dir[0]}`.slice(1);
-                    }
-                    else if (dir.length > 64) {
-                        const rows = [];
-                        const cols = [];
-                        dir.map((name) => {
-                            const nums = name.split(/[_\.]/);
-                            rows.push(+nums[0]);
-                            cols.push(+nums[1]);
-                        });
-                        const row = rows.sort((a,b)=>a-b)[Math.floor(rows.length / 2)];
-                        const col = cols.sort((a,b)=>a-b)[Math.floor(cols.length / 2)];
-                        const choice = dir.find((file) => RegExp(`${row}[^0-9]+${col}`).test(file));
-                        thumbnails.detail = `${path}/${choice}`.slice(1);
-                    }
-
-                    // Check if all thumbnails have been found
-                    if (!(thumbnails.overview && thumbnails.detail)) {
-                        idx++;
-                        findThumbnails();
-                    }
-                });
+        // Look through each directory to find thumbnails
+        let idx = 0;
+        const thumbnails = {overview: null, detail: null};
+        image.thumbnails = thumbnails;
+        function findThumbnails(){
+            if (idx === dir.length) {
+                return;
             }
-            findThumbnails();
-        });
+            const path = `./data/${fileDir}/${dir[idx]}`;
+            fs.readdir(path, (err, dir) => {
+                if (err) {
+                    // TODO: Handle errors
+                }
+
+                // Store suitable thumbnails
+                if (dir.length === 1) {
+                    thumbnails.overview = `${path}/${dir[0]}`.slice(1);
+                }
+                else if (dir.length > 64) {
+                    const rows = [];
+                    const cols = [];
+                    dir.map((name) => {
+                        const nums = name.split(/[_\.]/);
+                        rows.push(+nums[0]);
+                        cols.push(+nums[1]);
+                    });
+                    const row = rows.sort((a,b)=>a-b)[Math.floor(rows.length / 2)];
+                    const col = cols.sort((a,b)=>a-b)[Math.floor(cols.length / 2)];
+                    const choice = dir.find((file) => RegExp(`${row}[^0-9]+${col}`).test(file));
+                    thumbnails.detail = `${path}/${choice}`.slice(1);
+                }
+
+                // Check if all thumbnails have been found
+                if (!(thumbnails.overview && thumbnails.detail)) {
+                    idx++;
+                    findThumbnails();
+                }
+            });
+        }
+        findThumbnails();
     });
 }
 
@@ -104,8 +90,8 @@ function updateImages() {
         const images = [];
         uniqueNames.map((name) => images.push({name: name}));
         images.map((image) => {
-            getZLevels(image);
-            getThumbnails(image);
+            getZLevels(dir, image);
+            getThumbnails(dir, image);
         });
 
         availableImages = {images: images};
