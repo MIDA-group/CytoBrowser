421bb70841cfdfc7417ef9a77802f764da8bfc3c
Christopher Rydell <stoffe@kiosan.com>
2020-07-11 23:38:26 +0200

    Fixed scoping issues

--- public/js/markerPoints.js
+++ public/js/markerPoints.js
@@ -82,189 +82,199 @@ const markerPoints = (function (){
         }
     }
 
-    return ({
-        /**
-         * Add a single point to the data.
-         * @param {MarkerPoint} point A data representation of the point.
-         * @param {CoordSystem} [coordSystem="web"] Coordinate system used by the point.
-         * @param {boolean} [transmit=true] Any collaborators should also be
-         * told to add the point.
-         */
-        addPoint: function(point, coordSystem="web", transmit = true) {
-            const addedPoint = _clonePoint(point);
 
-            // Make sure the point has an id
-            if (addedPoint.id === undefined) {
+    /**
+     * Add a single point to the data.
+     * @param {MarkerPoint} point A data representation of the point.
+     * @param {CoordSystem} [coordSystem="web"] Coordinate system used by the point.
+     * @param {boolean} [transmit=true] Any collaborators should also be
+     * told to add the point.
+     */
+    function addPoint(point, coordSystem="web", transmit = true) {
+        const addedPoint = _clonePoint(point);
+
+        // Make sure the point has an id
+        if (addedPoint.id === undefined) {
+            addedPoint.id = _generateId();
+        }
+        else {
+            // If the id has been specified, check if it's not taken
+            const existingPoint = getPointById(addedPoint.id);
+            if (existingPoint !== undefined) {
+                console.info("Tried to assign an already-used id, reassigning.");
+                addedPoint.originalId = addedPoint.id;
                 addedPoint.id = _generateId();
             }
-            else {
-                // If the id has been specified, check if it's not taken
-                const existingPoint = getPointById(addedPoint.id);
-                if (existingPoint !== undefined) {
-                    console.info("Tried to assign an already-used id, reassigning.");
-                    addedPoint.originalId = addedPoint.id;
-                    addedPoint.id = _generateId();
-                }
-            }
-
-            // Store the coordinates in all systems and set the image coordinates
-            const coords = _getCoordSystems(addedPoint, coordSystem);
-            if (coordSystem != "image") {
-                addedPoint.x = coords.image.x;
-                addedPoint.y = coords.image.y;
-            }
-
-            // Store a data representation of the point
-            _points.push(addedPoint);
-
-            // Send the update to collaborators
-            transmit && collabClient.send({
-                type: "markerAction",
-                actionType: "add",
-                point: addedPoint
-            });
-
-            // Add a graphical representation of the point
-            overlayUtils.addTMCP(
-                addedPoint.id,
-                coords.viewport.x,
-                coords.viewport.y,
-                addedPoint.z,
-                addedPoint.mclass
-            );
-        },
-
-        /**
-         * Update the parameters of an already existing point.
-         * @param {number} id The initial id of the point to be updated.
-         * @param {MarkerPoint} point The new values for the point to be updated.
-         * @param {CoordSystem} [coordSystem="web"] Coordinate system used by the point.
-         * @param {boolean} [transmit=true] Any collaborators should also be
-         * told to update their point.
-         */
-        updatePoint: function(id, point, coordSystem="web", transmit = true) {
-            const points = _points;
-            const updatedIndex = points.findIndex((point) => point.id == id);
-            const updatedPoint = getPointById(id);
-
-            // Check if the point being updated exists first
-            if (updatedPoint === undefined) {
-                throw new Error("Tried to update a point that doesn't exist.");
-            }
-
-            // If the id is being changed, check if it's not taken
-            if (point.id !== undefined && point.id !== id) {
-                const existingPoint = getPointById(point.id);
-                if (existingPoint !== undefined) {
-                    console.info("Tried to assign an already-used id, keeping old id.");
-                    point.originalId = point.id;
-                    point.id = id;
-                }
-            }
-
-            // Copy over the updated properties
-            Object.assign(updatedPoint, point);
-
-            // Make sure the data is stored in the image coordinate system
-            const coords = _getCoordSystems(updatedPoint, coordSystem);
-            if (coordSystem != "image") {
-                updatedPoint.x = coords.image.x;
-                updatedPoint.y = coords.image.y;
-            }
-
-            // Store the point in data
-            points[updatedIndex] = updatedPoint;
-
-            // Send the update to collaborators
-            transmit && collabClient.send({
-                type: "markerAction",
-                actionType: "update",
-                id: id,
-                point: updatedPoint
-            });
-
-            // Update the point in the graphics
-            overlayUtils.updateTMCP(point.id, coords.viewport.x, coords.viewport.y, point.z, point.mclass);
-        },
-
-        /**
-         * Remove a point from the data.
-         * @param {number} id The id of the point to be removed.
-         * @param {boolean} [transmit=true] Any collaborators should also be
-         * told to remove the point.
-         */
-        removePoint: function(id, transmit = true) {
-            const points = _points;
-            const deletedIndex = points.findIndex((point) => point.id == id);
-
-            // Check if the point exists first
-            if (deletedIndex === -1) {
-                throw new Error("Tried to remove a point that doesn't exist");
-            }
-
-            // Remove the point from the data
-            points.splice(deletedIndex, 1);
-
-            // Send the update to collaborators
-            transmit && collabClient.send({
-                type: "markerAction",
-                actionType: "remove",
-                id: id
-            });
-
-            // Remove the point from the graphics
-            overlayUtils.removeTMCP(id);
-        },
-
-        /**
-         * Remove all points from the data.
-         * @param {boolean} [transmit=true] Any collaborators should also
-         * be told to clear their points.
-         */
-        clearPoints: function(transmit = true) {
-            const points = _points;
-            const ids = points.map((point) => point.id);
-            ids.forEach((id) => removePoint(id, false));
-
-            // Send the update to collaborators
-            transmit && collabClient.send({
-                type: "markerAction",
-                actionType: "clear",
-            });
-        },
-
-        /**
-         * Iterate a function for each point. The function will not change
-         * the values of the point, and will instead work on clones of them,
-         * effectively making them read-only. If the point values should be
-         * updated, updatePoint() can be run in the passed function.
-         * @param {function} f Function to be called with each point.
-         */
-        forEachPoint: function(f) {
-            _points.map(_clonePoint).forEach(f);
-        },
-
-        /**
-         * Get a copy of a specified point by its id.
-         * @param {number} id The id used for looking up the point.
-         * @returns {Object} A clone of the point with the specified id,
-         * or undefined if not in use.
-         */
-        getPointById: function(id) {
-            const point = _points.find((point) => point.id == id);
-            if (point === undefined) {
-                return undefined;
-            }
-            const pointClone = _clonePoint(point);
-            return pointClone;
-        },
-
-        /**
-         * Check whether or not the list of marker points is empty.
-         * @returns {boolean} Whether or not the list is empty.
-         */
-        empty: function() {
-            return _points.length === 0;
         }
-    })
+
+        // Store the coordinates in all systems and set the image coordinates
+        const coords = _getCoordSystems(addedPoint, coordSystem);
+        if (coordSystem != "image") {
+            addedPoint.x = coords.image.x;
+            addedPoint.y = coords.image.y;
+        }
+
+        // Store a data representation of the point
+        _points.push(addedPoint);
+
+        // Send the update to collaborators
+        transmit && collabClient.send({
+            type: "markerAction",
+            actionType: "add",
+            point: addedPoint
+        });
+
+        // Add a graphical representation of the point
+        overlayUtils.addTMCP(
+            addedPoint.id,
+            coords.viewport.x,
+            coords.viewport.y,
+            addedPoint.z,
+            addedPoint.mclass
+        );
+    },
+
+    /**
+     * Update the parameters of an already existing point.
+     * @param {number} id The initial id of the point to be updated.
+     * @param {MarkerPoint} point The new values for the point to be updated.
+     * @param {CoordSystem} [coordSystem="web"] Coordinate system used by the point.
+     * @param {boolean} [transmit=true] Any collaborators should also be
+     * told to update their point.
+     */
+    function updatePoint(id, point, coordSystem="web", transmit = true) {
+        const points = _points;
+        const updatedIndex = points.findIndex((point) => point.id == id);
+        const updatedPoint = getPointById(id);
+
+        // Check if the point being updated exists first
+        if (updatedPoint === undefined) {
+            throw new Error("Tried to update a point that doesn't exist.");
+        }
+
+        // If the id is being changed, check if it's not taken
+        if (point.id !== undefined && point.id !== id) {
+            const existingPoint = getPointById(point.id);
+            if (existingPoint !== undefined) {
+                console.info("Tried to assign an already-used id, keeping old id.");
+                point.originalId = point.id;
+                point.id = id;
+            }
+        }
+
+        // Copy over the updated properties
+        Object.assign(updatedPoint, point);
+
+        // Make sure the data is stored in the image coordinate system
+        const coords = _getCoordSystems(updatedPoint, coordSystem);
+        if (coordSystem != "image") {
+            updatedPoint.x = coords.image.x;
+            updatedPoint.y = coords.image.y;
+        }
+
+        // Store the point in data
+        points[updatedIndex] = updatedPoint;
+
+        // Send the update to collaborators
+        transmit && collabClient.send({
+            type: "markerAction",
+            actionType: "update",
+            id: id,
+            point: updatedPoint
+        });
+
+        // Update the point in the graphics
+        overlayUtils.updateTMCP(point.id, coords.viewport.x, coords.viewport.y, point.z, point.mclass);
+    },
+
+    /**
+     * Remove a point from the data.
+     * @param {number} id The id of the point to be removed.
+     * @param {boolean} [transmit=true] Any collaborators should also be
+     * told to remove the point.
+     */
+    function removePoint(id, transmit = true) {
+        const points = _points;
+        const deletedIndex = points.findIndex((point) => point.id == id);
+
+        // Check if the point exists first
+        if (deletedIndex === -1) {
+            throw new Error("Tried to remove a point that doesn't exist");
+        }
+
+        // Remove the point from the data
+        points.splice(deletedIndex, 1);
+
+        // Send the update to collaborators
+        transmit && collabClient.send({
+            type: "markerAction",
+            actionType: "remove",
+            id: id
+        });
+
+        // Remove the point from the graphics
+        overlayUtils.removeTMCP(id);
+    },
+
+    /**
+     * Remove all points from the data.
+     * @param {boolean} [transmit=true] Any collaborators should also
+     * be told to clear their points.
+     */
+    function clearPoints(transmit = true) {
+        const points = _points;
+        const ids = points.map((point) => point.id);
+        ids.forEach((id) => removePoint(id, false));
+
+        // Send the update to collaborators
+        transmit && collabClient.send({
+            type: "markerAction",
+            actionType: "clear",
+        });
+    },
+
+    /**
+     * Iterate a function for each point. The function will not change
+     * the values of the point, and will instead work on clones of them,
+     * effectively making them read-only. If the point values should be
+     * updated, updatePoint() can be run in the passed function.
+     * @param {function} f Function to be called with each point.
+     */
+    function forEachPoint(f) {
+        _points.map(_clonePoint).forEach(f);
+    },
+
+    /**
+     * Get a copy of a specified point by its id.
+     * @param {number} id The id used for looking up the point.
+     * @returns {Object} A clone of the point with the specified id,
+     * or undefined if not in use.
+     */
+    function getPointById(id) {
+        const point = _points.find((point) => point.id == id);
+        if (point === undefined) {
+            return undefined;
+        }
+        const pointClone = _clonePoint(point);
+        return pointClone;
+    },
+
+    /**
+     * Check whether or not the list of marker points is empty.
+     * @returns {boolean} Whether or not the list is empty.
+     */
+    function empty() {
+        return _points.length === 0;
+    }
+
+    // Return public members of the closure
+    return {
+        addPoint: addPoint,
+        updatePoint: updatePoint,
+        removePoint: removePoint,
+        clearPoints: clearPoints,
+        forEachPoint: forEachPoint,
+        getPointById: getPointById,
+        empty: empty
+    };
 })();
