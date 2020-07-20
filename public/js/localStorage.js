ab9c6b420747a2076ec21f1b58e6c139db7d9f58
Christopher Rydell <stoffe@kiosan.com>
2020-07-20 13:58:10 +0200

    Added localStorage.loadJSON()

--- public/js/localStorage.js
+++ public/js/localStorage.js
@@ -1,8 +1,10 @@
 /**
- * Handling the storage of markers on the local machine.
+ * Handling the storage of JSON data on the local machine.
  * @namespace localStorage
  */
 const localStorage = (function (){
+    const _fileInputId = "data_files_import";
+
     /**
      * General function for saving an arbitrary object as a JSON file
      * with UTF-8 encoding on the local machine.
@@ -16,6 +18,7 @@ const localStorage = (function (){
             throw new Error(`Expected an object, got a ${type} instead.`);
         }
 
+        // Create a hidden anchor element and use it to save data
         const dataJSON = encodeURIComponent(JSON.stringify(data, null, 4));
         const fileData = `text/json;charset=utf-8,${dataJSON}`;
         const a = document.createElement("a");
@@ -27,7 +30,32 @@ const localStorage = (function (){
         a.remove();
     }
 
+    /**
+     * Load a JSON file from the predetermined input element of the DOM.
+     * @returns {Promise<Object>} A promise that resolves with the loaded
+     * JSON file converted to an Object.
+     */
+    function loadJSON() {
+        const input = $(`#${_fileInputId}`);
+        if (!input.length) {
+            throw new Error("Unable to find specified file input element.");
+        }
+        if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+            throw new Error("The File APIs are not fully supported in this browser.");
+        }
+        const file = input.prop("files")[0];
+        if(!file){
+            alert("No file selected");
+            return;
+        }
+        if (!file.type.match('json')) {
+            alert("File should be json");
+            return;
+        }
+    }
+
     return {
-        saveJSON: saveJSON
+        saveJSON: saveJSON,
+        loadJSON: loadJSON
     };
 })();
