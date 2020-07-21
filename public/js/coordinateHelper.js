a07d8e45273467b117dd2b0602125b8410eda054
Christopher Rydell <stoffe@kiosan.com>
2020-07-21 17:17:24 +0200

    Added documentation to coordinateHelper

--- public/js/coordinateHelper.js
+++ public/js/coordinateHelper.js
@@ -1,6 +1,21 @@
+/**
+ * Namespace used for easily converting coordinates in the current image.
+ * In order to use the functions, the active image first has to be set
+ * with the {@link setImage} function. The different coordinate systems
+ * that the functions convert between are described in more detail
+ * {@link https://openseadragon.github.io/examples/viewport-coordinates/|here.}
+ * @namespace coordinateHelper
+ */
 const coordinateHelper = (function() {
     let _activeImage;
 
+    /**
+     * Convert from image coordinates to viewport coordinates.
+     * @param {Object} point A point in image coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in viewport coordinates.
+     */
     function imageToViewport({x, y}){
         if (!_activeImage) {
             throw new Error("Can't find coordinates without setting an image first.");
@@ -8,6 +23,13 @@ const coordinateHelper = (function() {
         return _activeImage.imageToViewportCoordinates(x, y);
     }
 
+    /**
+     * Convert from viewport coordinates to image coordinates.
+     * @param {Object} point A point in viewport coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in image coordinates.
+     */
     function viewportToImage({x, y}){
         if (!_activeImage) {
             throw new Error("Can't find coordinates without setting an image first.");
@@ -15,6 +37,13 @@ const coordinateHelper = (function() {
         return _activeImage.viewportToImageCoordinates(x, y);
     }
 
+    /**
+     * Convert from image coordinates to web coordinates.
+     * @param {Object} point A point in image coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in web coordinates.
+     */
     function imageToWeb({x, y}){
         if (!_activeImage) {
             throw new Error("Can't find coordinates without setting an image first.");
@@ -24,6 +53,13 @@ const coordinateHelper = (function() {
 
     }
 
+    /**
+     * Convert from web coordinates to image coordinates.
+     * @param {Object} point A point in web coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in image coordinates.
+     */
     function webToImage({x, y}){
         if (!_activeImage) {
             throw new Error("Can't find coordinates without setting an image first.");
@@ -33,10 +69,24 @@ const coordinateHelper = (function() {
 
     }
 
+    /**
+     * Convert from web coordinates to viewport coordinates.
+     * @param {Object} point A point in web coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in viewport coordinates.
+     */
     function webToViewport({x, y}){
         return webToImage(imageToViewport({x, y}));
     }
 
+    /**
+     * Convert from viewport coordinates to web coordinates.
+     * @param {Object} point A point in viewport coordinates.
+     * @param {number} point.x The x coordinate of the point.
+     * @param {number} point.y The y coordinate of the point.
+     * @returns {Object} The same point in web coordinates.
+     */
     function viewportToWeb({x, y}){
         return viewportToImage(imageToWeb({x, y}));
     }
