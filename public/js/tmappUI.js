c073c003d7fc3d37ddc9217cb00739d2de8b5687
Christopher Rydell <stoffe@kiosan.com>
2020-07-27 12:32:21 +0200

    Added non-functional checkbox for following collaborators

--- public/js/tmappUI.js
+++ public/js/tmappUI.js
@@ -207,18 +207,26 @@ const tmappUI = (function(){
             color.css("background-color", member.color);
             color.html("&nbsp;");
             const entry = $("<a></a>");
-            entry.addClass("list-group-item list-group-item-action");
+            const nameTag = $("<span></span>");
+            entry.addClass("list-group-item list-group-item-action d-flex justify-content-between align-items-center");
             if (member === localMember || !member.ready) {
                 entry.addClass("disabled");
             }
             entry.attr("href", "#");
-            entry.html(`&nbsp;&nbsp;&nbsp;${member.name}`);
-            entry.prepend(color);
+            nameTag.html(`&nbsp;&nbsp;&nbsp;${member.name}`);
+            nameTag.prepend(color);
+            entry.append(nameTag);
+            const follow = $("<span><input type='checkbox'></span>");
+            entry.append(follow);
             entry.click(event => {
                 event.preventDefault();
                 $("#collaboration_menu").modal("hide");
                 tmapp.moveTo(member.position);
             });
+            follow.click(event => {
+                event.stopPropagation();
+                console.log("Following!");
+            })
             $("#collaborator_list").append(entry);
         });
     }
