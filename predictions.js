55494e5a5c4a34173085f6bf1083df5e57280ee9
Christopher Rydell <stoffe@kiosan.com>
2021-02-03 13:59:09 +0100

    Added prepare() to predictions module

--- predictions.js
+++ predictions.js
@@ -6,11 +6,32 @@
 
 const fs = require("fs");
 const fsPromises = fs.promises;
+const convertPredictions = require("./convertPredictions");
 let predDir;
 
 function readJsonFromFile(path) {
     return fsPromises.readFile(path).then(data => {
         return JSON.parse(data);
+    });
+}
+
+/**
+ * Fill the specified prediction directory with results from the algorithm.
+ * This assumes that the results have already been produced.
+ * @param {Array<string>} images The names of all images for which
+ * results should be prepared.
+ * @param {string} zDir The directory where the algorithm has stored the
+ * focus-selected image patches.
+ * @param {string} csvDir The directory with CSV results
+ */
+function prepare(images, zDir, csvDir) {
+    images.forEach(image => {
+        convertPredictions.convertResultsToObject(image, zDir, csvDir)
+            .then(predictions => {
+                const filename = `${predDir}/${image}.json`;
+                const data = JSON.stringify(predictions);
+                fsPromises.writeFile(filename, data);
+            });
     });
 }
 
@@ -35,6 +56,7 @@ module.exports = function(dir) {
     }
     predDir = dir;
     return {
+        prepare: prepare,
         get: get
     };
 }
