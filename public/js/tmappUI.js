f7b8a2accd08b40d7105d1528ee5a9715c5f6502
Marcello <marcello.pietro@hotmail.com>
2022-03-21 07:24:34 +0100

    Included case of undefined prediction, return null in this case

--- public/js/tmappUI.js
+++ public/js/tmappUI.js
@@ -133,7 +133,7 @@ const tmappUI = (function(){
                 title: "Class prediction score",
                 minWidth: "6em",
                 displayFun: (elem, d) => {
-                    $(elem).html(d.prediction === null ? null : d.prediction.toFixed(4));
+                    $(elem).html((d.prediction === null || d.prediction === undefined) ? null : d.prediction.toFixed(4));
                 },
                 sortable: true
             },
