0db93a7be0097e7a258830d66af33fed51fc6e66
Joakim Lindblad <joakim@cb.uu.se>
2023-03-19 02:06:50 +0100

    FIX: Clear _imageStates FIX: Destroy _mouseHandler, was memory leak - to propagate Bugfix!

--- public/js/tmapp.js
+++ public/js/tmapp.js
@@ -711,6 +711,9 @@ const tmapp = (function() {
             v.element.remove(); //remove DOM node
             v.destroy();
         }
+        _imageStates=[];
+        _viewer=null;
+        _mouseHandler.destroy();
     }
     function _clearCurrentImage() {
         if (!_viewer) {
