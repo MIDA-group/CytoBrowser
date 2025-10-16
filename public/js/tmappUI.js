e50935efb8dd3aa3f21b909e49649c1665eab0f5
Joakim Lindblad <joakim@cb.uu.se>
2025-10-16 18:29:29 +0200

    Towards showing directories in the interface as well

--- public/js/tmappUI.js
+++ public/js/tmappUI.js
@@ -761,9 +761,9 @@ const tmappUI = (function(){
      * @param {Array<ImageDetails>} images Information about the images
      * being added.
      */
-    function updateImageBrowser(images) {
+    function updateImageBrowser(images, directories) {
         const container = $("#available_images");
-        htmlHelper.buildImageBrowser(container, images);
+        htmlHelper.buildImageBrowser(container, images, directories);
     }
 
     /**
